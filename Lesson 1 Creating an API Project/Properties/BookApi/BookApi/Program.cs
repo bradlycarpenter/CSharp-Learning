ac9defdd89eac2b8d0fe5207b63df13b9ec3b321
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-11-09 18:59:51 +0200

    update/web_api_working

---
+++ Lesson 1 Creating an API Project/Properties/BookApi/BookApi/Program.cs
@@ -0,0 +1,20 @@
+namespace BookApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // add services
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // add mapping
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
